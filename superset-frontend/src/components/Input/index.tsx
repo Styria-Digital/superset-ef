4b2a3edd4b452a8f1b46969df63bc6f11b34c7d6
Nikola Tucković <nikola.tuckovic@styria.dev>
2023-10-06 15:10:17 +0200

    initial implementation of TextAreaTranslatable, TODO transifex apply translations

--- superset-frontend/src/components/Input/index.tsx
+++ superset-frontend/src/components/Input/index.tsx
@@ -16,8 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import React, { useState } from 'react';
+
+import { useUiConfig } from 'src/components/UiConfigContext';
+
 import { styled } from '@superset-ui/core';
 import { Input as AntdInput, InputNumber as AntdInputNumber } from 'antd';
+
+// TODO: Remove this?
+// import { MainNav as Menu } from 'src/components/Menu';
+// import LanguagePicker from 'src/features/home/LanguagePicker';
+import { AntdForm } from 'src/components';
 
 export const Input = styled(AntdInput)`
   border: 1px solid ${({ theme }) => theme.colors.secondary.light3};
@@ -33,3 +42,85 @@ export const TextArea = styled(AntdInput.TextArea)`
   border: 1px solid ${({ theme }) => theme.colors.secondary.light3};
   border-radius: ${({ theme }) => theme.borderRadius}px;
 `;
+
+const StyledFormItem = styled(AntdForm.Item)`
+  margin-bottom: 0;
+`;
+
+interface Props {
+  name: string;
+  label: string;
+  rows?: number;
+  value?: string;
+}
+
+export const TextAreaTranslatable: React.FC<Props> = ({
+  name,
+  label,
+  rows = 3,
+  value = '',
+  ...props
+}) => {
+  const uiConfig = useUiConfig();
+
+  // TODO remove this?
+  // const [shownLangField, setShownLangField] = useState<string>(uiConfig.currentLocale);
+
+  // const showTranslation = (mainFieldName: string, langKey: string) => {
+  //   setShownLangField(langKey);
+  // };
+
+  const otherLanguages = Object.fromEntries(
+    Object.entries(uiConfig.availableLanguages || {}).filter(
+      ([key]) => key !== uiConfig.defaultLocale,
+    ),
+  );
+
+  return (
+    <div>
+      {/* TODO remove this? */}
+      {/* <Menu mode="horizontal">
+        <LanguagePicker
+          locale={uiConfig.currentLocale}
+          languages={uiConfig.availableLanguages}
+          mainFieldName="description"
+          callback={showTranslation}
+        />
+      </Menu> */}
+      <StyledFormItem label={label} name={name}>
+        <TextArea
+          rows={rows}
+          style={{ maxWidth: '100%' }}
+          name={name}
+          id={name}
+          defaultValue={value}
+        />
+      </StyledFormItem>
+      <div style={{ padding: '8px 0px' }}>Translations</div>
+      {/* TODO: implement send to Transifex upon save/change */}
+      {uiConfig.availableLanguages &&
+        Object.entries(otherLanguages).map(([lang, config]) => (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              paddingBottom: '10px',
+            }}
+          >
+            <span className="f16">
+              <i
+                className={`flag ${config.flag}`}
+                style={{ marginRight: '5px' }}
+              />
+              {config.name}
+            </span>
+            <TextArea
+              rows={rows}
+              style={{ maxWidth: '100%' }}
+              name={`${name}_${lang}`}
+            />
+          </div>
+        ))}
+    </div>
+  );
+};
